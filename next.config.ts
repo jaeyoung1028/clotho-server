65a7063b24b4ece763ccfef960a64045593dac7b
jaeyoung <ijaeyeong1028@gmail.com>
2026-04-23 20:46:22 +0900

    config 일단 변경

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  rewrites: async () => {
+    return {
+      beforeFiles: [
+        {
+          source: '/api/server/tarot/chat',
+          destination: 'https://clotho-server-vyw7.vercel.app/api/tarot',
+        }
+      ]
+    }
+  }
 };
 
 export default nextConfig;
